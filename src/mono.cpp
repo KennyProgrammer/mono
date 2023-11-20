14d7c5a466a81c08c4955aa2e8fb482fd25faaf6
Danil <kennytuts@gmail.com>
2023-11-21 01:44:16 +0300

    Add mono.cpp to generate Mono.lib with linkage of mono-2.0-sgen.lib.

---
+++ src/mono.cpp
@@ -0,0 +1,11 @@
+
+//
+// NOTE: This file is not part of Mono Project. This file used only to generate Mono.lib
+// with linkage of mono-2.0-sgen.lib.
+//
+
+#include "mono/jit/jit.h"
+#include "mono/metadata/appdomain.h"
+#include "mono/metadata/assembly.h"
+#include "mono/metadata/class.h"
+#include "mono/metadata/object.h"
